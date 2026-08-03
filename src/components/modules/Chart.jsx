145d9044c42155bfa842250545cfb1867c359a4e
Pr82 <parsa.rohani82@gmail.com>
2026-08-03 16:33:23 +0330

    added an exit button for exiting from the modal and created a state to use that for the convertion

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,16 +1,20 @@
 import styles from "./Chart.module.css";
+import { convertData } from "../../helpers/ConvertData";
+import { useState } from "react";
 
 function Chart({ chart, setChart }) {
+  const [type, setType] = useState("prices");
+  convertData(chart, type);
   const closeHandler = () => {
     setChart(null);
   };
 
   return (
     <div className={styles.container}>
-      <span className={styles.close} onClick={closeHandler}>x</span>
-      <div className={styles.chart}>
-
-      </div>
+      <span className={styles.close} onClick={closeHandler}>
+        x
+      </span>
+      <div className={styles.chart}></div>
     </div>
   );
 }
