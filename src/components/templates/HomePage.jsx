396e91e3ad9b5535ee8bc488b30f6d3f040f5699
Pr82 <parsa.rohani82@gmail.com>
2026-08-03 16:28:02 +0330

    fixed the bug in the Chart component that wasn't showing the chart value

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -34,7 +34,7 @@ function HomePage() {
       <Search currency={currency} setCurrency={setCurrency} />
       <TableCoin coins={coins} isLoading={isLoading} currency={currency} setChart={setChart} />
       <Pagination page={page} setPage={setPage} />
-      {chart && <Chart setChart={setChart}/>}
+      {chart && <Chart chart={chart} setChart={setChart}/>}
     </div>
   );
 }
